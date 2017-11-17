13a09fcb9cea0d1f41e9cac99a342d3ef047cbd5
Wes Reid <bwreid@gmail.com>
2017-11-16 17:49:20 -0800

    Update server with new error messages and to serve only as an api

--- server.js
+++ server.js
@@ -4,33 +4,27 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
 
-const app = express()
+const app = express();
 app.use(bodyParser.json());
-// enable static 'public' directory for serving html/css/js files to client
-app.use(express.static(path.join('public')));
 
-const snacks = require('./routes/snacks')
-app.use(snacks)
+const snacks = require('./routes/snacks');
+app.use('/api', snacks);
 
 app.use((req, res) => {
-  res.sendStatus(404);
+  const status = 404;
+  const message = `Could not ${req.method} ${req.path}`;
+  res.status(status).json({ status, message });
 });
 
-// Handle Boom errors
 app.use((err, _req, res, _next) => {
-  if (err.output && err.output.statusCode) {
-    return res
-      .status(err.output.statusCode)
-      .set('Content-Type', 'text/plain')
-      .send(err.message);
-  }
-
-  console.error(err.stack);
-  res.sendStatus(500);
+  console.error(err);
+  const status = err.status || 500;
+  const message = err.message || 'Something went wrong!';
+  res.status(status).json({ message, status });
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('listening on port', port)
-})
+  console.log('listening on port', port);
+});
